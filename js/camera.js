d52370f0a8c5c357c4f6231bdcdbc11fc99601c8
HOST <host.michael@gmail.com>
2013-03-16 11:59:16 +0100

    camera

--- js/camera.js
+++ js/camera.js
@@ -23,8 +23,8 @@ function Settings() {
     
     // Photo quality and editing options:
     this.quality = 40;                                          // cameraOptions: quality
-    this.targetWidth = 500;                                     // cameraOptions: targetWidth
-    this.targetHeight = 500;                                    // cameraOptions: targetHeight
+    this.targetWidth = 200;                                     // cameraOptions: targetWidth
+    this.targetHeight = 200;                                    // cameraOptions: targetHeight
     this.allowEdit = true;                                      // cameraOptions: allowEdit
     this.correctOrientation = true;                             // cameraOptions: correctOrientation
     
